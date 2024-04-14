0abef32d906dded7aea21c7c05d301c849b81161
Abhishek Chauhan <abhishekprogrammer18112003@gmail.com>
2024-04-14 15:09:08 +0530

    initialize Project by Abhi

---
+++ android/app/src/main/kotlin/com/abhion/noteefy/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.abhion.noteefy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/constants/url_constants.dart
@@ -0,0 +1,4 @@
+// ignore_for_file: constant_identifier_names, unnecessary_brace_in_string_interps
+
+const String BASE_URL = "https://dev.pickaservice.com";
+
